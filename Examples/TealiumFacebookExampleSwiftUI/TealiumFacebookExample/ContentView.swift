1d3ee9b67601de1850d4bde18f47cf4de8bf2278
Christina Sund <christina@tealium.com>
2019-11-14 16:19:42 -0800

    Updated sample apps (#2)

--- Examples/TealiumFacebookExampleSwiftUI/TealiumFacebookExample/ContentView.swift
+++ Examples/TealiumFacebookExampleSwiftUI/TealiumFacebookExample/ContentView.swift
@@ -9,16 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    var profile: Profile
+    var product: Product
+    var purchase: Purchase
     var body: some View {
         VStack {
            TitleView().padding(.bottom, 30)
             VStack(spacing: 30) {
-                ButtonView(event: "Set User")
-                ButtonView(event: "Update User Value")
-                ButtonView(event: "Achieve Level")
-                ButtonView(event: "Completed Registration")
-                ButtonView(event: "Log Product Item")
-                ButtonView(event: "Log Purchase") 
+                ButtonView(event: "Set User", ["customer_id": profile.customerId])
+                ButtonView(event: "Update User Value", ["customer_name": profile.customerName])
+                ButtonView(event: "Achieve Level", ["event_object": ["level": profile.level]])
+                ButtonView(event: "Completed Registration", ["event_object": ["registration_method": profile.registrationMethod]])
+                ButtonView(event: "Log Product Item", product.data)
+                ButtonView(event: "Log Purchase", purchase.data)
                 ButtonView(event: "Flush")
             }
             Spacer()
@@ -31,19 +34,19 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             
-           ContentView()
+            ContentView(profile: Profile.default, product: Product.default, purchase: Purchase.default)
                .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
                .previewDisplayName("iPhone 8")
             
-           ContentView()
+           ContentView(profile: Profile.default, product: Product.default, purchase: Purchase.default)
                .previewDevice(PreviewDevice(rawValue: "iPhone X"))
                .previewDisplayName("iPhone X")
 
-           ContentView()
+           ContentView(profile: Profile.default, product: Product.default, purchase: Purchase.default)
               .previewDevice(PreviewDevice(rawValue: "iPhone XS Max"))
               .previewDisplayName("iPhone XS Max")
             
-            ContentView()
+            ContentView(profile: Profile.default, product: Product.default, purchase: Purchase.default)
                .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch)"))
                .previewDisplayName("iPad Pro (11-inch)")
         }
